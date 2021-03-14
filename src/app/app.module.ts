20584719c450a29dffe420936cac9eec8e8ae049
ruslanen <ruslan68a@gmail.com>
2021-03-14 14:38:41 +0300

    Init. Chapter 7.

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from "./store/store.module";
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [BrowserModule, StoreModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
